3167e3b179877276fcfe1f2ebff0a8a80d818f0a
Daniel Vogel <hello@dnlvgl.com>
2021-01-03 18:05:44 +0100

    udate config

--- init.el
+++ init.el
@@ -1,16 +1,17 @@
 ;;
-;; barebone init.el, load config in literate orgmode file 
+;; barebone init.el, load config in literate orgmode file
 ;;
-
-;; load packages
-(package-initialize t)
-(setq package-enable-at-startup nil)
 
 ;; load emacs config in orgmode file
 (require 'org)
 (org-babel-load-file "~/.emacs.d/emacs.org")
-;;(org-babel-load-file "~/.emacs.d/secrets.org")
+
+;; alt/new way of loading
+;;(org-babel-load-file
+;; (expand-file-name
+;;  "emacs.org"
+;;  user-emacs-directory))
 
 ;; keep emacs custom-settings in separate file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file)
+
