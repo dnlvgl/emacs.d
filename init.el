6f54b6605bb8cfcd34c47131069d4deca0548c16
Daniel Vogel <hello@dnlvgl.com>
2021-04-22 23:10:06 +0200

    make lsp and vue behave

--- init.el
+++ init.el
@@ -13,5 +13,17 @@
 ;;  user-emacs-directory))
 
 ;; keep emacs custom-settings in separate file
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+;; (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(lsp-ui-sideline-show-code-actions nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
